1e5615923dc7d1cacdbc8faafcfcb30eb1d9bbea
gclt7777 <zhaoxinman07@163.com>
2025-10-31 20:54:08 +0800

    Restore RKSTAEA as function handle entry point

--- Algorithms/RKS-TAEA/Point2LineDistance.m
+++ Algorithms/RKS-TAEA/Point2LineDistance.m
@@ -1,10 +1,15 @@
-function d = Point2LineDistance(P, A, B)
+function d = Point2LineDistance(P,A,B)
 
     d = zeros(size(P,1),1);
-    for i = 1:size(P,1)
+    for i = 1 : size(P,1)
         pa = P(i,:) - A;
         ba = B - A;
-        t = dot(pa, ba)/dot(ba, ba);
-        d(i,1) = norm(pa - t * ba,2);
+        denom = dot(ba,ba);
+        if denom == 0
+            d(i,1) = norm(pa,2);
+        else
+            t = dot(pa,ba)/denom;
+            d(i,1) = norm(pa - t*ba,2);
+        end
     end
 end
