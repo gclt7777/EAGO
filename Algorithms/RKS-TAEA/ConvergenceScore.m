ef65b6568d3f304c01d25cd2b3e7b6cef8bc8e8a
gclt7777 <zhaoxinman07@163.com>
2025-11-05 13:04:44 +0800

    Guard normalization for degenerate extremes

--- Algorithms/RKS-TAEA/ConvergenceScore.m
+++ Algorithms/RKS-TAEA/ConvergenceScore.m
@@ -1,10 +1,14 @@
-function CrowdDis = ConvergenceScore(front, p)
+function CrowdDis = ConvergenceScore(front,p)
 
+    [m,~] = size(front);
+    CrowdDis = zeros(1,m);
 
-[m,~] = size(front);
-CrowdDis = zeros(1,m);
-
-for i=1:m
-    CrowdDis(i) = 1./norm(front(i,:),p);
+    for i = 1 : m
+        value = norm(front(i,:),p);
+        if value > 0
+            CrowdDis(i) = 1./value;
+        else
+            CrowdDis(i) = inf;
+        end
+    end
 end
-end 
--- Algorithms/RKS-TAEA/FindCornerSolutions.m
+++ Algorithms/RKS-TAEA/FindCornerSolutions.m
@@ -1,21 +1,23 @@
-function [indexes] = FindCornerSolutions(front)
+function indexes = FindCornerSolutions(front)
 
+    [m,n] = size(front);
+    if m == 0
+        indexes = [];
+        return;
+    end
 
-[m,n] = size(front);
+    % let's normalize the objectives
+    if m <= n
+        indexes = 1:m;
+        return;
+    end
 
-%% let's normalize the objectives
-if m<=n
-  indexes = 1:m;
-  return
-end
-
-%% let's define the axes of the n-dimensional spaces 
-W = zeros(n)+1e-6+eye(n);
-[r,~]= size(W);
-indexes = zeros(1,n);
-for i=1:r
-   [~, index] = min(Point2LineDistance(front, zeros(1,n), W(i,:)));
-   indexes(i) = index;
-end
-
+    % let's define the axes of the n-dimensional spaces
+    W = eye(n) + 1e-6;
+    indexes = zeros(1,n);
+    for i = 1 : n
+        [~,index] = min(Point2LineDistance(front,zeros(1,n),W(i,:)));
+        indexes(i) = index;
+    end
+    indexes = unique(indexes,'stable');
 end
